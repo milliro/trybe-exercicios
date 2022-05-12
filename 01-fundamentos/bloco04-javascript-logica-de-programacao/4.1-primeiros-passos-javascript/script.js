095fe583a24d5cb5e006fddd19bafd55f198d423
milliro <mrosmoura@gmail.com>
2022-05-12 18:57:40 -0300

    Concluindo exercício 2

--- 01-fundamentos/bloco04-javascript-logica-de-programacao/4.1-primeiros-passos-javascript/script.js
+++ 01-fundamentos/bloco04-javascript-logica-de-programacao/4.1-primeiros-passos-javascript/script.js
@@ -1,7 +1,8 @@
-// Faça cinco programas, um para cada operação aritmética básica. 
+// 1- Faça cinco programas, um para cada operação aritmética básica. 
 // Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados.
 const a = 10
-const b = 2
+const b = 20
+const c = 6
 
 console.log('Adição (a + b)')
 console.log(a + b)
@@ -21,6 +22,15 @@ console.log('')
 
 console.log('Módulo (a % b)')
 console.log(a % b)
+console.log('')
 
 ///////////
 ///////////
+
+// 2 - Faça um programa que retorne o maior de dois números. 
+// Defina no começo do programa duas constantes com os valores que serão comparados.
+
+console.log('Retorne o maior de dois números')
+const higherNumber = a > b ? a : b
+console.log(`O maior número entre ${a} e ${b} é "${higherNumber}"`)
+console.log('')
