d536d7be4df2ae91afffe2a44c17c0310dcabfbb
milliro <mrosmoura@gmail.com>
2022-05-12 18:46:34 -0300

    Concluindo exercício 1

---
+++ 01-fundamentos/bloco04-javascript-logica-de-programacao/4.1-primeiros-passos-javascript/script.js
@@ -0,0 +1,26 @@
+// Faça cinco programas, um para cada operação aritmética básica. 
+// Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados.
+const a = 10
+const b = 2
+
+console.log('Adição (a + b)')
+console.log(a + b)
+console.log('')
+
+console.log('Subtração (a - b)')
+console.log(a - b)
+console.log('')
+
+console.log('Multiplicação (a * b)')
+console.log(a * b)
+console.log('')
+
+console.log('Divisão (a / b)')
+console.log(a / b)
+console.log('')
+
+console.log('Módulo (a % b)')
+console.log(a % b)
+
+///////////
+///////////
