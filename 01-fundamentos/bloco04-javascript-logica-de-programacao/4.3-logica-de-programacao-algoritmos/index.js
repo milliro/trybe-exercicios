9b9883a10e97bb586bd1a936121c95acc12e2e5e
milliro <mrosmoura@gmail.com>
2022-05-16 19:32:14 -0300

    Concluindo exercício 3

--- 01-fundamentos/bloco04-javascript-logica-de-programacao/4.3-logica-de-programacao-algoritmos/index.js
+++ 01-fundamentos/bloco04-javascript-logica-de-programacao/4.3-logica-de-programacao-algoritmos/index.js
@@ -15,12 +15,46 @@ console.log('')
 //     Utilize a string abaixo como exemplo, depois troque por outras para 
 //     verificar se seu algoritmo está funcionando corretamente.
 
-let word = 'banana';
-const wordArray = word.split('');
+let word = 'reviver';
+const letters = word.split('');
 
 let invertedWord = ''
-for (let index = 0; index < wordArray.length; index += 1) {
-  invertedWord += wordArray[wordArray.length - 1 - index]
+for (let index = 0; index < letters.length; index += 1) {
+  invertedWord += letters[letters.length - 1 - index]
 }
 console.log(invertedWord)
 invertedWord = ''
+console.log('')
+
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+// 3 - Considere o array de strings abaixo:
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. 
+// Considere o número de caracteres de cada palavra.
+
+let charNumbers = 0
+let biggestWord = ''
+
+for (let indexArray = 0; indexArray < array.length; indexArray += 1) {
+  charNumbers = array[indexArray].length
+  
+  for (let index = 0; index < array.length; index++) {
+    if (charNumbers < array[index].length) {
+      charNumbers = array[index].length
+      biggestWord = array[index]
+    }
+  }
+}
+
+console.log(`A maior palavra da array é ${biggestWord} contendo ${charNumbers} caracteres`)
+console.log('')
+
+charNumbers = 0
+biggestWord = ''
+
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
