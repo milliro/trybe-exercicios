544c630bd41ad9ac2e0286ea29b4e5b742af7023
milliro <mrosmoura@gmail.com>
2022-05-16 14:02:30 -0300

    Concluindo exercício 2

--- 01-fundamentos/bloco04-javascript-logica-de-programacao/4.2-primeiros-passo-js/script.js
+++ 01-fundamentos/bloco04-javascript-logica-de-programacao/4.2-primeiros-passo-js/script.js
@@ -6,10 +6,30 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
 
 // 1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
 
-// for(let index = 0; index < numbers.length; index += 1) {
-//   console.log(numbers[index])
-// }
+console.log(`Nesse primeiro exercício, percorra o array imprimindo 
+todos os valores nele contidos com a função console.log();`)
+
+for(let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index])
+}
+
+console.log('')
 
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
 
+// 2 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+console.log(`Para o segundo exercício, some todos os 
+valores contidos no array e imprima o resultado;`)
+
+let soma = 0
+
+for(let index = 0; index < numbers.length; index += 1) {
+  soma += numbers[index]
+}
+console.log(soma)
+console.log('')
+
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
