fc808ddde81a8d38cf485faea0837f1b0a3a42cb
milliro <mrosmoura@gmail.com>
2022-05-17 18:50:14 -0300

    Concluindo exercício 5

--- 01-fundamentos/bloco04-javascript-logica-de-programacao/4.4-objetos-funcoes/index.js
+++ 01-fundamentos/bloco04-javascript-logica-de-programacao/4.4-objetos-funcoes/index.js
@@ -46,3 +46,18 @@ console.log('')
 
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 
+// 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. 
+// Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+
+let infoTwo = {
+  personagem: 'Tio Patinhas',
+  origem: `Christmas on Bear Mountain, Dell's Four Color Comics #178`,
+  nota: 'O último MacPatinhas',
+};
+
+console.log(`${info.personagem} e ${infoTwo.personagem}
+${info.origem} e ${infoTwo.origem}
+${info.nota} e ${infoTwo.nota}
+Ambos recorrentes`)
