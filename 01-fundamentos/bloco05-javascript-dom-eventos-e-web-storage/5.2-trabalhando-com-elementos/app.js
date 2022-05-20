bf0884d96c1103744888456dc7a2a2891837d243
milliro <mrosmoura@gmail.com>
2022-05-20 18:31:19 -0300

    Concluindo exercícios 3 e 4

--- 01-fundamentos/bloco05-javascript-dom-eventos-e-web-storage/5.2-trabalhando-com-elementos/app.js
+++ 01-fundamentos/bloco05-javascript-dom-eventos-e-web-storage/5.2-trabalhando-com-elementos/app.js
@@ -4,11 +4,21 @@ const title = document.createElement('h1');
 title.innerText = 'Exercício 5.2 - JavaScript DOM';
 
 
-const main = document.createElement('main')
-main.classList.add('main-content')
+const main = document.createElement('main');
+main.classList.add('main-content');
 
-// insert to body
-body.appendChild(title)
-body.appendChild(main)
 
-console.log(main)
+const section = document.createElement('section');
+section.classList.add('center-content')
+
+const paragraph = document.createElement('p')
+paragraph.innerText = 'Algum texto.'
+// insert to body;
+body.appendChild(title);
+body.appendChild(main);
+
+// insert to main;
+main.appendChild(section);
+
+//insert to section;
+section.appendChild(paragraph)
