cc2493dd19535c903f0ce983aa1a494cba79816c
milliro <mrosmoura@gmail.com>
2022-05-20 18:35:21 -0300

    Concluindo exercícios 5 e 6

--- 01-fundamentos/bloco05-javascript-dom-eventos-e-web-storage/5.2-trabalhando-com-elementos/app.js
+++ 01-fundamentos/bloco05-javascript-dom-eventos-e-web-storage/5.2-trabalhando-com-elementos/app.js
@@ -13,12 +13,21 @@ section.classList.add('center-content')
 
 const paragraph = document.createElement('p')
 paragraph.innerText = 'Algum texto.'
+
+const leftSection = document.createElement('section');
+leftSection.classList.add('left-content')
+
+const rightSection = document.createElement('section');
+rightSection.classList.add('right-content')
+
 // insert to body;
 body.appendChild(title);
 body.appendChild(main);
 
 // insert to main;
 main.appendChild(section);
+main.appendChild(leftSection);
+main.appendChild(rightSection);
 
 //insert to section;
 section.appendChild(paragraph)
