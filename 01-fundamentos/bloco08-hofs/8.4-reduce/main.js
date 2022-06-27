845682adb88d15617d083df6fa4fae34d0dad2e1
milliro <mrosmoura@gmail.com>
2022-06-27 19:07:25 -0300

    Concluindo exercícios 1 e 2

---
+++ 01-fundamentos/bloco08-hofs/8.4-reduce/main.js
@@ -0,0 +1,97 @@
+// 1 - Dada uma matriz, transforme em um array.
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+const flatten = (array) => {
+  return array.reduce((acc, curr) => {
+    let newArray = [];
+    newArray.push(...acc, ...curr)
+    return newArray;
+  })
+};
+
+// Para os exercícios 2, 3 e 4 considere o seguinte array:
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// Adicione o código do exercício aqui:
+
+// 2 - Crie uma string com os nomes de todas as pessoas autoras.
+
+const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+
+const reduceNames = () => {
+  const authors = books.reduce((acc, curr) => {
+    acc.push(curr.author.name);
+    return acc
+  }, []);
+  
+  const expectedResult = authors.reduce((acc, curr) => `${acc}, ${curr}`)
+
+  return `${expectedResult}.`;
+}
